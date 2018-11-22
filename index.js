532ea88f6bcc703bb37c4d322a8e61fac16f6974
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-22 14:17:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+
+const drivers = []
+drivers.length = 0;
+drivers.push('Milo', 'Otis', 'Garfield')
+function destructivelyAppendDriver(name){
+  let x = drivers
+  if (name === 'Ralph'){
+    x = drivers.push('Ralph')
+  }
+  return x
+}
+function destructivelyPrependDriver(name2) {
+  let y = drivers
+  if (name2 === 'Bob'){
+    y = drivers.unshift('Bob')
+  }
+  return y
+}
+
+function destructivelyRemoveLastDriver(m) {
+  let z = drivers
+  if (m=1){
+    z = drivers.pop()
+  }
+  return z
+}
+function destructivelyRemoveFirstDriver(b) {
+  let fir = drivers
+  if (b=1){
+    fir = drivers.shift()
+  }
+  return fir
+}
